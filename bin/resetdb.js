8d4b06801c5133405d76f25231b1141b6ee51473
easydoesit <michael.grills@gmail.com>
2023-05-01 13:20:40 -0600

    moved db into api

--- bin/resetdb.js
+++
@@ -1,8 +0,0 @@
-// load .env data into process.env
-require('dotenv').config();
-
-// other dependancies
-const fs = require('fs');
-const chalk = require('chalk');
-const db = require('../db/connection');
-
