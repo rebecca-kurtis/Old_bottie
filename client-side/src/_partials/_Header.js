1a655cea82edb92d44a5cf35d3e432ee4002f8d4
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-05-02 22:24:45 -0700

    Add Header code and styling, and overall brand styling

--- client-side/src/_partials/_Header.js
+++ client-side/src/_partials/_Header.js
@@ -1,9 +1,25 @@
 import React from "react";
+import logo from "../Bottie logo (1).png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+
+
+import "./Header.css"
 
 export default function Header() {
   return(
     <header>
-      This is my Header
+      <img src={logo} alt="Bottie Logo" className="logo"/>
+      <nav className="nav">
+          <a className="nav-link active" aria-current="page" href="/">Get Started with Bottie</a>
+          <a classNam="nav-link" href="/products">All Plants</a>
+      </nav>
+      <div className="nav-icons">
+        <FontAwesomeIcon className="icon" icon={faUser} />
+        <FontAwesomeIcon className="icon" icon={faCartShopping}/>
+      </div>
     </header>
   );
 };
--- client-side/src/pages/Home.js
+++ client-side/src/pages/Home.js
@@ -13,6 +13,12 @@ export default function Home() {
         <Link to="/products">
           All plants
         </Link>
+        <p>
+        Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+        Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+        Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+
+        </p>
       </section>
     </main>
   );
