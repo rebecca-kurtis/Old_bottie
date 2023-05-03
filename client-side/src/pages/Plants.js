f8c27de2fa5c0999df4981344366dd24bc58521f
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-05-02 23:00:33 -0700

    Add Products page code updates and styles

--- client-side/src/pages/Plants.js
+++ client-side/src/pages/Plants.js
@@ -1,16 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment } from "react";
+import HeroBanner from "../components/HeroBanner";
+import PageTitle from "../components/PageTitle";
+import ProductGrid from "../components/ProductsGrid";
+import './Plants.css';
 
-export default function Plants() {
+export default function Plants(props) {
   return (
-    <main>
-      <section className="pages">
-        <h1>This all my plants</h1>
-        <Link to="/products/id">
-          A plant detailed page
-        </Link>
-      </section>
-    </main>
+    <Fragment classNames="container">
+      <HeroBanner
+        message={"Find the perfect gift from our carefully selected plants collection."}
+      />
+      <PageTitle
+        message={"My plants selection"}
+      />
+      <ProductGrid/>
+      {/* <ProductCard
+        name={"Azalea"}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+        price={"22.95$"}
+        imageSrc={"/images/1.png"}
+      /> */}
+    </Fragment>
   );
 };
 
